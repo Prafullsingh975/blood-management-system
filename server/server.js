e639689ffe99bc385c1149d8abb6c1894e152a21
Prafull Singh <prafullsingh975@gmail.com>
2023-10-28 23:46:36 +0530

    Production

--- server/server.js
+++ server/server.js
@@ -23,7 +23,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(cors());
+app.use(
+  cors({
+    origin: ["https://blood-management-system-one.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
